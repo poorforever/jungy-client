f3b4fe3f6cb4324ef8647870333ed292336f9a84
Anaël <anaelmanzambi@gmail.com>
2025-12-11 00:52:04 +0100

    Setting up menu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {Home} from './components/home/home';
+import {News} from './components/news/news';
+import {Contact} from './components/contact/contact';
+import {About} from './components/about/about';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+      path: '',
+      children: [
+        {
+          path: 'home',
+          component: Home,
+          title: 'Home'
+        },
+        {
+          path: 'news',
+          component: News,
+          title: 'News'
+        },
+        {
+          path: 'contact',
+          component: Contact,
+          title: 'Contact'
+        },
+        {
+          path: 'about',
+          component: About,
+          title: 'About'
+        }
+      ]
+    }
+  ];
